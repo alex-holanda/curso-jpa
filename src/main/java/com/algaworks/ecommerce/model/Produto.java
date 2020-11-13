559e9be685f7e354d74e743a2b9cf677ec3a13e0
Alex Holanda <alex.holanda@outlook.com>
2020-11-13 10:59:46 -0300

    Configuração inicial e criado o util para iniciar unidade de persistencia

---
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -0,0 +1,30 @@
+package com.algaworks.ecommerce.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	
+	private String descricao;
+	
+	private BigDecimal preco;
+}
