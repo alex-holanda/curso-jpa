2becdeff21f2b9e96c01231b4ccd20d27cab5169
Alex Holanda <alex.holanda@outlook.com>
2020-11-16 17:06:53 -0300

    Utilizado o persist para dentro de uma transação para inserir uma informação no banco de dados

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -1,10 +1,35 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
 
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
+	@Test
+	public void inserirPrimeiroObjeto() {
+		Produto produto = new Produto();
+		produto.setNome("Câmera Canon");
+		produto.setDescricao("A melhor definição para suas fotos.");
+		produto.setPreco(new BigDecimal("5000"));
+		
+		entityManager.getTransaction().begin();
+		
+		entityManager.persist(produto);
+		
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificado = entityManager.find(Produto.class, 2);
+		
+		Assert.assertNotNull(produtoVerificado);
+	}
+	
 	public void abrirEFecharATransacao() {
 //		Somente para o método não mostrar erro
 		Produto produto = new Produto();
