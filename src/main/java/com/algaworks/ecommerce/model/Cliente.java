65aac7af647a15819cec57bad0a226753d080e1d
Alex Holanda <alex.holanda@outlook.com>
2020-11-17 14:11:47 -0300

    Criado Entidade Cliente e a classe PrimeiroCRUD para testes

---
+++ src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -0,0 +1,18 @@
+package com.algaworks.ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	private Integer id;
+	
+	private String nome;
+}
---
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/PrimeiroCrudTest.java
@@ -0,0 +1,66 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+
+public class PrimeiroCrudTest extends EntityManagerTest {
+
+//	CRUD
+	
+	@Test
+	public void createClienteTest() {
+		Cliente cliente = new Cliente();
+		cliente.setId(3);
+		cliente.setNome("Guilherme Boulos");
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		Cliente clienteVerificado = entityManager.find(Cliente.class, cliente.getId());
+		assertNotNull(clienteVerificado);
+	}
+	
+	@Test
+	public void readClientTtest() {
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+		assertNotNull(cliente);
+	}
+	
+	@Test
+	public void updateClienteTest() {
+		Cliente cliente = new Cliente();
+		cliente.setId(2);
+		cliente.setNome("Bruno Covas");
+		
+		entityManager.getTransaction().begin();
+		entityManager.merge(cliente);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Cliente clienteVerificado = entityManager.find(Cliente.class, 2);
+		assertEquals("Bruno Covas", clienteVerificado.getNome());
+	}
+	
+	@Test
+	public void deleteClienteTest() {
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+		
+		entityManager.getTransaction().begin();
+		entityManager.remove(cliente);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Cliente clienteVerificado = entityManager.find(Cliente.class, cliente.getId());
+		assertNull(clienteVerificado);
+	}
+}
