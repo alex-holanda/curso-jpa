d22e6a4605255f5a113e93116d32c5fba04d8ccf
Alex Holanda <alex.holanda@outlook.com>
2020-11-18 14:50:20 -0300

    Categoria configurada para utilizar o identificador por tabela

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -5,8 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 import lombok.Data;
 
@@ -16,8 +16,9 @@ import lombok.Data;
 public class Categoria {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-	@SequenceGenerator(name = "seq", sequenceName = "categoria_sequencia_chave_primaria")
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
+	@TableGenerator(name = "tabela", table = "hibernate_sequences", pkColumnName = "sequence_name", pkColumnValue = "categoria",
+			valueColumnName = "next_val", initialValue = 0, allocationSize = 50)
 	private Integer id;
 	
 	private String nome;
