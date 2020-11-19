2a1c3f86bf14d8d4193baada6d2be67de069cfc5
Alex Holanda <alex.holanda@outlook.com>
2020-11-19 16:16:56 -0300

    Criado relacionamento entre nota fiscal e pedido

--- src/test/java/com/algaworks/ecommerce/relacionamento/RelacionanemtoOneToOneTest.java
+++ src/test/java/com/algaworks/ecommerce/relacionamento/RelacionanemtoOneToOneTest.java
@@ -1,9 +1,12 @@
 package com.algaworks.ecommerce.relacionamento;
 
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.NotaFiscal;
 import com.algaworks.ecommerce.model.PagamentoCartao;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.StatusPagamento;
@@ -29,5 +32,22 @@ public class RelacionanemtoOneToOneTest extends EntityManagerTest {
 		Assert.assertNotNull(pedidoVerificacao.getPagamento());
 	}
 	
-	
+	@Test
+	public void verificarRelacionamentoNotaFiscalPedido() {
+		Pedido pedido = entityManager.find(Pedido.class, 1);
+		
+		NotaFiscal notaFiscal = new NotaFiscal();
+		notaFiscal.setDataEmissao(new Date());
+		notaFiscal.setXml("notafiscalxml");
+		notaFiscal.setPedido(pedido);
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(notaFiscal);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+		Assert.assertNotNull(pedidoVerificacao.getNotaFiscal());
+	}
 }
