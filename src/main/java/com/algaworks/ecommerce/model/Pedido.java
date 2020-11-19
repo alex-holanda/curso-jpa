e9c91f157a062b2931d8b793da7d5a17b4810fca
Alex Holanda <alex.holanda@outlook.com>
2020-11-19 16:09:02 -0300

    Feito relacionamento de um para um entre pagamento cartao e pedido

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -51,4 +52,7 @@ public class Pedido {
 	
 	@OneToMany(mappedBy = "pedido")
 	private List<ItemPedido> itens;
+	
+	@OneToOne(mappedBy = "pedido")
+	private PagamentoCartao pagamento;
 }
