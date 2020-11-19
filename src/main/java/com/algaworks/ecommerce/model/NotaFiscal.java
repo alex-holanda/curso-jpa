5c116c472d59c089f9ecdb72a3ec4aae4fdc0f9d
Alex Holanda <alex.holanda@outlook.com>
2020-11-19 17:01:09 -0300

    Utilizado o JoinTable entre nota fiscal e pedido

--- src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -22,8 +23,11 @@ public class NotaFiscal {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+//	@JoinColumn(name = "pedido_id")
 	@OneToOne
-	@JoinColumn(name = "pedido_id")
+	@JoinTable(name = "pedido_nota_fiscal",
+			joinColumns = @JoinColumn(name = "nota_fiscal_id", unique = true),
+			inverseJoinColumns = @JoinColumn(name = "pedido_id", unique = true))
 	private Pedido pedido;
 	
 	private String xml;
