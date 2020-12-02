6a7308eb8f249697ec83311b969f2696d573f97f
Alex Holanda <alex.holanda@outlook.com>
2020-12-02 10:54:39 -0300

    Customizado o nome da chave estrangeira

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -58,7 +59,7 @@ public class Pedido extends EntidadeBaseInteger {
 	private Endereco endereco;
 
 	@ManyToOne(optional = false)
-	@JoinColumn(name = "cliente_id")
+	@JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
 	private Cliente cliente;
 
 	@OneToMany(mappedBy = "pedido")
