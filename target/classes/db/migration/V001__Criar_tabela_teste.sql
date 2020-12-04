ab1a5adf824e94579251ed3a7b3faa8392d43fc2
Alex Holanda <alex.holanda@outlook.com>
2020-12-04 10:21:41 -0300

    Entendendo as operações em  cascata

---
+++ target/classes/db/migration/V001__Criar_tabela_teste.sql
@@ -0,0 +1,13 @@
+CREATE TABLE teste (
+	id bigint not null auto_increment,
+    nome varchar(80) not null,
+    
+    primary key (id)
+);
+
+insert into teste (nome) values ('Teste 1');
+insert into teste (nome) values ('Teste 2');
+insert into teste (nome) values ('Teste 3');
+insert into teste (nome) values ('Teste 4');
+insert into teste (nome) values ('Teste 5');
+insert into teste (nome) values ('Teste 6');
