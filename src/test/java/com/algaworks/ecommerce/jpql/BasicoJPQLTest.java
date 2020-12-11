aaa87a113bf4acf57859976fab3e6f99b47e7c49
Alex Holanda <alex.holanda@outlook.com>
2020-12-11 12:01:00 -0300

    Diferenças e vantagens ao utilizar o JPQL

---
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -0,0 +1,29 @@
+package com.algaworks.ecommerce.jpql;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+
+public class BasicoJPQLTest extends EntityManagerTest {
+
+	@Test
+	public void buscarPorIdentificador() {
+//		JAVA Persistence Query Language - JPQL
+		
+//		JPQL - select p from Pedido p where p.id = 1 - Referência a entidade
+		
+//		SQL - select p.* from pedido p where p.id = 1 - Referência a tabela do banco de dados
+		
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p where p.id = 1", Pedido.class);
+		
+		Pedido pedido = typedQuery.getSingleResult();
+		Assert.assertNotNull(pedido);
+		
+//		List<Pedido> pedidoList = typedQuery.getResultList();
+//		Assert.assertFalse(pedidoList.isEmpty());
+	}
+}
