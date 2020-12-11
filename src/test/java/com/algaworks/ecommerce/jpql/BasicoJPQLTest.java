56eaa0b9b7931a980312ff39317ff5abc759c49b
Alex Holanda <alex.holanda@outlook.com>
2020-12-11 13:10:59 -0300

    Diferenças entre Query e TypedQuery

--- src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -1,5 +1,6 @@
 package com.algaworks.ecommerce.jpql;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.junit.Assert;
@@ -9,6 +10,23 @@ import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Pedido;
 
 public class BasicoJPQLTest extends EntityManagerTest {
+	
+	@Test
+	public void mostrarDiferencaQueries() {
+		String jpql = "select p from Pedido p where p.id = 1";
+		
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+		Pedido pedido1 = typedQuery.getSingleResult();
+		Assert.assertNotNull(pedido1);
+		
+		Query query = entityManager.createQuery(jpql);
+		Pedido pedido2 = (Pedido) query.getSingleResult();
+		Assert.assertNotNull(pedido2);
+		
+//		List<Pedido> lista = query.getResultList();
+//		Assert.assertFalse(lista.isEmpty());
+		
+	}
 
 	@Test
 	public void buscarPorIdentificador() {
