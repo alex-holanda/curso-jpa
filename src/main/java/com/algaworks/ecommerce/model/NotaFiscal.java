5becaab96dedc37af19b816e8f24d9a21d27a24c
Alex Holanda <alex.holanda@outlook.com>
2020-12-14 13:50:45 -0300

    Passando parâmetros para as consultas

--- src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -10,6 +10,8 @@ import javax.persistence.Lob;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -29,6 +31,7 @@ public class NotaFiscal extends EntidadeBaseInteger {
 	@Lob
 	private byte[] xml;
 	
+	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "data_emissao", nullable = false)
 	private Date dataEmissao;
 }
