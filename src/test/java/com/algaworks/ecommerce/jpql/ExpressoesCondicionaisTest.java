667c1c3d210e86d382f7fa8985dba33a9de9a015
Alex Holanda <alex.holanda@outlook.com>
2020-12-15 13:50:32 -0300

    Utilizando diferente (<>) em consultas

--- src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -1,82 +1,90 @@
 package com.algaworks.ecommerce.jpql;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import javax.persistence.TypedQuery;
-
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.algaworks.ecommerce.EntityManagerTest;
+import javax.persistence.TypedQuery;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
 
-	@Test
-	public void usarBetween() {
-		String jpql = "select p from Produto p where p.preco between :precoInicial and :precoFinal";
+    @Test
+    public void usarExpressaoDiferente() {
+        String jpql = "select p from Produto p where p.preco <> 100";
 
-		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
-		typedQuery.setParameter("precoInicial", new BigDecimal("400"));
-		typedQuery.setParameter("precoFinal", new BigDecimal("1500"));
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
 
-		List<Object[]> lista = typedQuery.getResultList();
-		Assert.assertFalse(lista.isEmpty());
-	}
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 
-	@Test
-	public void usarMaiorMenorComData() {
-		String jpql = "select p from Pedido p where p.dataCriacao > :dataCriacao";
+    @Test
+    public void usarBetween() {
+        String jpql = "select p from Produto p where p.preco between :precoInicial and :precoFinal";
 
-		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
-		typedQuery.setParameter("dataCriacao", LocalDateTime.now().minusDays(2));
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        typedQuery.setParameter("precoInicial", new BigDecimal("400"));
+        typedQuery.setParameter("precoFinal", new BigDecimal("1500"));
 
-		List<Object[]> lista = typedQuery.getResultList();
-		Assert.assertFalse(lista.isEmpty());
-	}
-	
-	@Test
-	public void usarMaiorMenor() {
-		String jpql = "select p from Produto p where p.preco >= :precoInicial and p.preco <= :precoFinal";
-		
-		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
-		typedQuery.setParameter("precoInicial", new BigDecimal("400"));
-		typedQuery.setParameter("precoFinal", new BigDecimal("1500"));
-		
-		List<Object[]> lista = typedQuery.getResultList();
-		Assert.assertFalse(lista.isEmpty());
-	}
-	
-	@Test
-	public void usarIsNull() {
-		String jpql = "select p from Produto p where p.foto is null";
-		
-		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
-		
-		List<Object[]> lista = typedQuery.getResultList();
-		Assert.assertFalse(lista.isEmpty());
-	}
-	
-	@Test
-	public void usarIsEmpty() {
-		String jpql = "select p from Produto p where p.categorias is empty";
-		
-		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
-		
-		List<Object[]> lista = typedQuery.getResultList();
-		Assert.assertFalse(lista.isEmpty());
-	}
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 
-	@Test
-	public void usarExpressaoCondicionalLike() {
-		String jpql = "select c from Cliente c where c.nome like concat('%', :nome, '%')";
-		
-		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
-		typedQuery.setParameter("nome", "a");
-		
-		List<Object[]> lista = typedQuery.getResultList();
-		Assert.assertFalse(lista.isEmpty());
-	}
+    @Test
+    public void usarMaiorMenorComData() {
+        String jpql = "select p from Pedido p where p.dataCriacao > :dataCriacao";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        typedQuery.setParameter("dataCriacao", LocalDateTime.now().minusDays(2));
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarMaiorMenor() {
+        String jpql = "select p from Produto p where p.preco >= :precoInicial and p.preco <= :precoFinal";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        typedQuery.setParameter("precoInicial", new BigDecimal("400"));
+        typedQuery.setParameter("precoFinal", new BigDecimal("1500"));
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarIsNull() {
+        String jpql = "select p from Produto p where p.foto is null";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarIsEmpty() {
+        String jpql = "select p from Produto p where p.categorias is empty";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarExpressaoCondicionalLike() {
+        String jpql = "select c from Cliente c where c.nome like concat('%', :nome, '%')";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        typedQuery.setParameter("nome", "a");
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 }
