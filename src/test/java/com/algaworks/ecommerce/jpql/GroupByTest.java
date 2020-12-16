9d72bc95cf399a7cfdfd415126616c9eee77c4d3
Alex Holanda <alex.holanda@outlook.com>
2020-12-16 15:56:22 -0300

    Exercício usando group by

--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -19,8 +19,18 @@ public class GroupByTest extends EntityManagerTest {
 //                + " group by year(p.dataCriacao), month(p.dataCriacao)";
 
 //        Total de vendas por categoria
-        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c"
-                + " group by c.id";
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c"
+//                + " group by c.id";
+
+//        Total de vendas por cliente
+//        String jpql = "select cli.nome, sum(ip.precoProduto) from ItemPedido ip join ip.pedido p join ip.produto pro"
+//                + " join p.cliente cli group by cli.id";
+
+//        Total de vendas por dia e por categoria
+        String jpql = "select concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
+                " concat(c.nome, ' ', sum(ip.precoProduto)) from ItemPedido ip join ip.pedido p"
+                + " join ip.produto pro join pro.categorias c"
+                + " group by c.id, year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
