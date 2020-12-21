1771786e4661dbdce45ec4bb43693c829c2f270b
Alex Holanda <alex.holanda@outlook.com>
2020-12-21 15:57:43 -0300

    Utilizando o Join Fetch

--- src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
@@ -2,6 +2,7 @@ package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.*;
+import com.mysql.cj.xdevapi.Client;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,6 +11,28 @@ import javax.persistence.criteria.*;
 import java.util.List;
 
 public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void usarJoinFetch() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        root.fetch("cliente");
+        root.fetch("notaFiscal", JoinType.LEFT);
+        root.fetch("pagamento", JoinType.LEFT);
+//        Join<Pedido, Cliente> joinCliente = (Join<Pedido, Cliente>) root.<Pedido, Cliente>fetch("cliente");
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        Pedido pedido = typedQuery.getSingleResult();
+        Assert.assertNotNull(pedido);
+        Assert.assertFalse(pedido.getItens().isEmpty());
+
+    }
 
     @Test
     public void fazerLeftOuterJoin() {
