0f0fd02a3adc745fbd9f87958844e7e57c92d389
Alex Holanda <alex.holanda@outlook.com>
2021-01-13 13:41:46 -0300

    Exercício usando group by

--- src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
@@ -12,6 +12,37 @@ import java.util.List;
 public class GroupByCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void agruparResultado03Exercicio() {
+//        Total de vendas por cliente
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                "join ip.pedido p join p.cliente c" +
+//                "group by c.id"
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+        Join<ItemPedido, Pedido> joinPedido = root.join(ItemPedido_.pedido);
+        Join<Pedido, Cliente> joinCliente = joinPedido.join(Pedido_.cliente);
+
+        criteriaQuery.multiselect(
+                joinCliente.get(Cliente_.nome),
+                criteriaBuilder.sum(root.get(ItemPedido_.precoProduto))
+        );
+
+        criteriaQuery.groupBy(joinCliente.get(Cliente_.id));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                "Nome: " + arr[0]
+                + ", Sum: " + arr[1]
+        ));
+    }
+
+    @Test
     public void agruparResultado02() {
 //        Total de vendas por categoria
 //        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
@@ -66,7 +97,7 @@ public class GroupByCriteriaTest extends EntityManagerTest {
 
         lista.forEach(arr -> System.out.println(
                 "Categoria:" + arr[0]
-                + ", Count: " + arr[1]
+                        + ", Count: " + arr[1]
         ));
     }
 }
