c26db91e5a348c6d209eb1122d6c491a4d974704
Alex Holanda <alex.holanda@outlook.com>
2021-01-15 15:25:04 -0300

    Criando subqueries com all

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,6 +1,7 @@
 insert into produto (id, nome, preco, data_criacao, descricao) values (1, 'Kindle', 799.0, date_sub(sysdate(), interval 1 day), 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
-insert into produto (id, nome, preco, data_criacao, descricao) values (3, 'Câmera GoPro Hero 7', 1400.0, date_sub(sysdate(), interval 1 day), 'Desempenho 2x melhor.');
+insert into produto (id, nome, preco, data_criacao, descricao) values (3, 'Câmera GoPro Hero 7', 1500.0, date_sub(sysdate(), interval 1 day), 'Desempenho 2x melhor.');
 insert into produto (id, nome, preco, data_criacao, descricao) values (4, 'Câmera Canon 80D', 3500.0, sysdate(), 'O melhor ajuste de foco.');
+insert into produto (id, nome, preco, data_criacao, descricao) values (5, 'Microfone de Lapela', 50.0, sysdate(), 'Produto massa');
 
 insert into cliente (id, nome, cpf) values (1, 'Fernando Medeiros', '000');
 insert into cliente (id, nome, cpf) values (2, 'Marcos Mariano', '111');
@@ -27,7 +28,7 @@ insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (2, 'PROCESSANDO', 'cartao', '4567', null);
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras, data_vencimento) values (3, 'RECEBIDO', 'boleto', null, '8910', date_sub(sysdate(), interval 2 day));
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (4, 'PROCESSANDO', 'cartao', '1112', null);
-insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras, data_vencimento) values (6, 'PROCESSANDO', 'boleto', null, '67890', date_add(sysdate(), interval 2 day));
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras, data_vencimento) values (6, 'PROCESSANDO', 'boleto', null, '456', date_add(sysdate(), interval 2 day));
 
 insert into nota_fiscal (pedido_id, xml, data_emissao) values (2, '<xml />', sysdate());
 
