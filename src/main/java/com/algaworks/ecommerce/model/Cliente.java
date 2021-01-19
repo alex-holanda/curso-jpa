ced36f0ada89b6ab4d85e8b81cad9dd6121922ae
Alex Holanda <alex.holanda@outlook.com>
2021-01-19 11:02:40 -0300

    Configurando uma procedure com a anotação @NamedStoredProcedureQuery

--- src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -4,23 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.ForeignKey;
-import javax.persistence.Index;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapKeyColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.PostLoad;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.SecondaryTable;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -32,6 +16,12 @@ import lombok.Setter;
 		@UniqueConstraint(name = "unq_cpf", columnNames = { "cpf" }) }, indexes = {
 				@Index(name = "idx_nome", columnList = "nome") })
 @SecondaryTable(name = "cliente_detalhe", pkJoinColumns = @PrimaryKeyJoinColumn(name = "cliente_id"), foreignKey = @ForeignKey(name = "fk_cliente_detalhes_cliente"))
+@NamedStoredProcedureQuery(name = "compraram_acima_media", procedureName = "compraram_acima_media",
+	parameters = {
+		@StoredProcedureParameter(name = "ano", type = Integer.class, mode = ParameterMode.IN)
+	},
+	resultClasses = Cliente.class
+)
 public class Cliente extends EntidadeBaseInteger {
 
 	@Column(length = 100, nullable = false)
--- src/test/java/com/algaworks/ecommerce/consultasnativas/StoredProceduresTest.java
+++ src/test/java/com/algaworks/ecommerce/consultasnativas/StoredProceduresTest.java
@@ -13,6 +13,17 @@ import java.util.List;
 public class StoredProceduresTest extends EntityManagerTest {
 
     @Test
+    public void chamarNamedStoredProcedure() {
+        StoredProcedureQuery storedProcedureQuery = entityManager.createNamedStoredProcedureQuery("compraram_acima_media");
+
+        storedProcedureQuery.setParameter("ano", 2021);
+
+        List<Cliente> lista = storedProcedureQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
     public void atualizarPrecoProdutoExercicio() {
         StoredProcedureQuery storedProcedureQuery = entityManager.createStoredProcedureQuery("ajustar_preco_produto");
 
