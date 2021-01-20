bba3b33d56d76299f8520fa7341af579af144883
Alex Holanda <alex.holanda@outlook.com>
2021-01-20 11:08:16 -0300

    Corrigir erro do OneToOne Lazy do hibernate

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -1,134 +1,170 @@
 package com.algaworks.ecommerce.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
+import com.algaworks.ecommerce.listener.GerarNotaFiscalListener;
+import com.algaworks.ecommerce.service.GenericoListener;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.engine.spi.PersistentAttributeInterceptable;
+import org.hibernate.engine.spi.PersistentAttributeInterceptor;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.ForeignKey;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.PostLoad;
-import javax.persistence.PostPersist;
-import javax.persistence.PostRemove;
-import javax.persistence.PostUpdate;
-import javax.persistence.PrePersist;
-import javax.persistence.PreRemove;
-import javax.persistence.PreUpdate;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.PositiveOrZero;
-
-import com.algaworks.ecommerce.listener.GerarNotaFiscalListener;
-import com.algaworks.ecommerce.service.GenericoListener;
-
-import lombok.Getter;
-import lombok.Setter;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "pedido")
-@EntityListeners({ GerarNotaFiscalListener.class, GenericoListener.class })
-public class Pedido extends EntidadeBaseInteger {
+@EntityListeners({GerarNotaFiscalListener.class, GenericoListener.class})
+public class Pedido extends EntidadeBaseInteger implements PersistentAttributeInterceptable {
 
-	@PastOrPresent
-	@NotNull
-	@Column(name = "data_criacao", updatable = false, nullable = false)
-	private LocalDateTime dataCriacao;
+    @PastOrPresent
+    @NotNull
+    @Column(name = "data_criacao", updatable = false, nullable = false)
+    private LocalDateTime dataCriacao;
 
-	@PastOrPresent
-	@Column(name = "data_ultima_atualizacao", insertable = false)
-	private LocalDateTime dataUltimaAtualizacao;
+    @PastOrPresent
+    @Column(name = "data_ultima_atualizacao", insertable = false)
+    private LocalDateTime dataUltimaAtualizacao;
 
-	@PastOrPresent
-	@Column(name = "data_conclusao")
-	private LocalDateTime dataConclusao;
+    @PastOrPresent
+    @Column(name = "data_conclusao")
+    private LocalDateTime dataConclusao;
 
-	@OneToOne(mappedBy = "pedido")
-	private NotaFiscal notaFiscal;
+    @LazyToOne(LazyToOneOption.NO_PROXY)
+    @OneToOne(mappedBy = "pedido", fetch = FetchType.LAZY)
+    private NotaFiscal notaFiscal;
 
-	@NotNull
-	@PositiveOrZero
-	private BigDecimal total;
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal total;
 
-	@NotNull
-	@Column(length = 30, nullable = false)
-	@Enumerated(EnumType.STRING)
-	private StatusPedido status;
+    @NotNull
+    @Column(length = 30, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
 
-	@Embedded
-	private Endereco endereco;
+    @Embedded
+    private Endereco endereco;
 
-	@NotNull
-	@ManyToOne(optional = false)
-	@JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
-	private Cliente cliente;
+    @NotNull
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
+    private Cliente cliente;
 
-	@NotEmpty
-	@OneToMany(mappedBy = "pedido")
-	private List<ItemPedido> itens;
+    @NotEmpty
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
 
-	@OneToOne(mappedBy = "pedido")
-	private Pagamento pagamento;
+    @LazyToOne(LazyToOneOption.NO_PROXY)
+    @OneToOne(mappedBy = "pedido", fetch = FetchType.LAZY)
+    private Pagamento pagamento;
 
-	private void calcularTotal() {
-		total = BigDecimal.ZERO;
+    private void calcularTotal() {
+        total = BigDecimal.ZERO;
 
-		if (itens != null) {
-			total = itens.stream()
-					.map(i -> new BigDecimal(i.getQuantidade()).multiply(i.getPrecoProduto()))
-					.reduce(BigDecimal.ZERO, BigDecimal::add);
-		}
-	}
+        if (itens != null) {
+            total = itens.stream()
+                    .map(i -> new BigDecimal(i.getQuantidade()).multiply(i.getPrecoProduto()))
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+        }
+    }
 
-	@PrePersist
-	public void aoPersistir() {
-		dataCriacao = LocalDateTime.now();
-		calcularTotal();
-	}
+    @PrePersist
+    public void aoPersistir() {
+        dataCriacao = LocalDateTime.now();
+        calcularTotal();
+    }
 
-	@PreUpdate
-	public void aoAtualizar() {
-		dataUltimaAtualizacao = LocalDateTime.now();
-		calcularTotal();
-	}
+    @PreUpdate
+    public void aoAtualizar() {
+        dataUltimaAtualizacao = LocalDateTime.now();
+        calcularTotal();
+    }
 
-	@PostPersist
-	public void aposPersistir() {
-		System.out.println(">>> Após persistir Pedido");
-	}
+    @PostPersist
+    public void aposPersistir() {
+        System.out.println(">>> Após persistir Pedido");
+    }
 
-	@PostUpdate
-	public void aposAtualizar() {
-		System.out.println(">>> Após atualizar Pedido");
-	}
+    @PostUpdate
+    public void aposAtualizar() {
+        System.out.println(">>> Após atualizar Pedido");
+    }
 
-	@PreRemove
-	public void aoRemover() {
-		System.out.println(">>> Antes de remover Pedido ");
-	}
+    @PreRemove
+    public void aoRemover() {
+        System.out.println(">>> Antes de remover Pedido ");
+    }
 
-	@PostRemove
-	public void aposRemover() {
-		System.out.println(">>> Após remover Pedido");
-	}
+    @PostRemove
+    public void aposRemover() {
+        System.out.println(">>> Após remover Pedido");
+    }
 
-	@PostLoad
-	public void aoCarregar() {
-		System.out.println("Após carregar o Pedido");
-	}
+    @PostLoad
+    public void aoCarregar() {
+        System.out.println("Após carregar o Pedido");
+    }
 
-	public boolean isPago() {
-		return StatusPedido.PAGO.equals(status);
-	}
+    public boolean isPago() {
+        return StatusPedido.PAGO.equals(status);
+    }
+
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
+    @Transient
+    private PersistentAttributeInterceptor persistentAttributeInterceptor;
+
+    @Override
+    public PersistentAttributeInterceptor $$_hibernate_getInterceptor() {
+        return this.persistentAttributeInterceptor;
+    }
+
+    @Override
+    public void $$_hibernate_setInterceptor(PersistentAttributeInterceptor interceptor) {
+        this.persistentAttributeInterceptor = interceptor;
+    }
+
+    public NotaFiscal getNotaFiscal() {
+        if (this.persistentAttributeInterceptor != null) {
+            return (NotaFiscal) persistentAttributeInterceptor
+                    .readObject(this, "notaFiscal", this.notaFiscal);
+        }
+        return this.notaFiscal;
+    }
+
+    public void setNotaFiscal(NotaFiscal notaFiscal) {
+        if (this.persistentAttributeInterceptor != null) {
+            this.notaFiscal = (NotaFiscal) persistentAttributeInterceptor
+                    .writeObject(this, "notaFiscal", this.notaFiscal, notaFiscal);
+        } else {
+            this.notaFiscal = notaFiscal;
+        }
+    }
+
+    public Pagamento getPagamento() {
+        if (this.persistentAttributeInterceptor != null) {
+            return (Pagamento) persistentAttributeInterceptor
+                    .readObject(this, "pagamento", this.pagamento);
+        }
+        return this.pagamento;
+    }
+
+    public void setPagamento(Pagamento pagamento) {
+        if (this.persistentAttributeInterceptor != null) {
+            this.pagamento = (Pagamento) persistentAttributeInterceptor
+                    .writeObject(this, "pagamento", this.pagamento, pagamento);
+        } else {
+            this.pagamento = pagamento;
+        }
+    }
 }
