ad7c00414e3ef16225a49303cbb3a1f4e2751eb5
Alex Holanda <alex.holanda@outlook.com>
2021-01-22 10:10:38 -0300

    Verificando se uma entidade está no chache

--- src/test/java/com/algaworks/ecommerce/cache/CacheTest.java
+++ src/test/java/com/algaworks/ecommerce/cache/CacheTest.java
@@ -2,10 +2,7 @@ package com.algaworks.ecommerce.cache;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Pedido;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 import javax.persistence.Cache;
 import javax.persistence.EntityManager;
@@ -24,6 +21,21 @@ public class CacheTest {
     @AfterClass
     public static void tearDownAfterClass() {
         entityManagerFactory.close();
+    }
+
+    @Test
+    public void verificarSeEstaNoCache() {
+        Cache cache = entityManagerFactory.getCache();
+
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+
+        System.out.println(">>> Buscando a partir da instância 1:");
+        entityManager1
+                .createQuery("select p from Pedido p", Pedido.class)
+                .getResultList();
+
+        Assert.assertTrue(cache.contains(Pedido.class, 1));
+        Assert.assertTrue(cache.contains(Pedido.class, 2));
     }
 
     @Test
