9035d8bf87fc366579a68cdd5241adb31b6b3b60
Alex Holanda <alex.holanda@outlook.com>
2021-01-22 10:05:49 -0300

    Removendo entidades do cache

--- src/test/java/com/algaworks/ecommerce/cache/CacheTest.java
+++ src/test/java/com/algaworks/ecommerce/cache/CacheTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.persistence.Cache;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -23,6 +24,28 @@ public class CacheTest {
     @AfterClass
     public static void tearDownAfterClass() {
         entityManagerFactory.close();
+    }
+
+    @Test
+    public void removerDoCache() {
+        Cache cache = entityManagerFactory.getCache();
+
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+        System.out.println(">>> Buscando a partir da instância 1:");
+        entityManager1
+                .createQuery("select p from Pedido p", Pedido.class)
+                .getResultList();
+
+        System.out.println(">>> Removendo instância do cache:");
+//        cache.evict(Pedido.class, 1);
+//        cache.evict(Pedido.class);
+        cache.evictAll(); //Remove tudo do cache
+
+        System.out.println(">>> Buscando a partir da instância 2:");
+        entityManager2.find(Pedido.class, 1);
+        entityManager2.find(Pedido.class, 2);
     }
 
     @Test
