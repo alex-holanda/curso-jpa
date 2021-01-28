18df5809e58490c81bb6076470f74ee7e03a55f8
Alex Holanda <alex.holanda@outlook.com>
2021-01-28 11:05:56 -0300

    O lock vai ser colocado na TypedQuery, funcionando do mesmo modo para o JPQL ou a Criteria API

--- src/test/java/com/algaworks/ecommerce/concorrencia/LockPessimistaTest.java
+++ src/test/java/com/algaworks/ecommerce/concorrencia/LockPessimistaTest.java
@@ -10,6 +10,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.LockModeType;
 import javax.persistence.Persistence;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
 
 public class LockPessimistaTest {
 
@@ -35,6 +37,89 @@ public class LockPessimistaTest {
         try {
             Thread.sleep(segundos * 1000);
         } catch (InterruptedException e) { }
+    }
+
+    @Test
+    public void usarLockNaTypedQuery() {
+        Runnable runnable1 = () -> {
+            log("Iniciando Runnable 01.");
+
+            EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+            entityManager1.getTransaction().begin();
+
+            String novaDescricao = "Descrição detalhada. CTM: " + System.currentTimeMillis();
+
+            log("Runnable 01 vai carregar todos os produtos.");
+            List<Produto> lista = entityManager1
+                    .createQuery("select p from Produto p")
+                    .setLockMode(LockModeType.PESSIMISTIC_READ)
+                    .getResultList();
+
+            Produto produto = lista
+                    .stream()
+                    .filter(p -> p.getId().equals(1))
+                    .findFirst()
+                    .get();
+
+            log("Runnable 01 vai alterar o produto ID igual a 1.");
+            produto.setDescricao(novaDescricao);
+
+            log("Runnable 01 vai esperar por 3 segundos");
+            esperar(3);
+
+            log("Runnable 01 vai confirmar a trasação.");
+            entityManager1.getTransaction().commit();
+            entityManager1.close();
+
+            log("Encerrando Runnable 01.");
+        };
+
+        Runnable runnable2 = () -> {
+            EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+            entityManager2.getTransaction().begin();
+
+            log("Iniciando Runnable 02.");
+
+            String novaDescricao = "Descrição massa! CTM: " + System.currentTimeMillis();
+
+            log("Runnable 02 vai carregar o produto 1.");
+            Produto produto = entityManager2.find(Produto.class, 1, LockModeType.PESSIMISTIC_WRITE);
+
+            log("Runnable 02 vai alterar o produto.");
+            produto.setDescricao(novaDescricao);
+
+            log("Runnable 02 vai esperar por 1 segundos.");
+            esperar(1);
+
+            log("Runnable 01 vai confirmar a transação");
+            entityManager2.getTransaction().commit();
+            entityManager2.close();
+
+            log("Encerrando Runnable 02.");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+
+        esperar(1);
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        EntityManager entityManager3 = entityManagerFactory.createEntityManager();
+        Produto produto = entityManager3.find(Produto.class, 1);
+        entityManager3.close();
+
+        Assert.assertTrue(produto.getDescricao().startsWith("Descrição detalhada."));
+
+        log("Encerrando método de teste");
     }
 
     @Test
