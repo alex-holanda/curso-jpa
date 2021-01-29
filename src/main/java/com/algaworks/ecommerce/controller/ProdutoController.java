b2d7fa406f26d1be6a0e368d5b942d711850c9d3
Alex Holanda <alex.holanda@outlook.com>
2021-01-29 14:23:32 -0300

    Implementado os outros métodos do controller produtos

--- src/main/java/com/algaworks/ecommerce/controller/ProdutoController.java
+++ src/main/java/com/algaworks/ecommerce/controller/ProdutoController.java
@@ -1,12 +1,16 @@
 package com.algaworks.ecommerce.controller;
 
+import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.repository.Produtos;
 import com.algaworks.ecommerce.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/produtos")
@@ -17,6 +21,40 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+
+    @PostMapping("/{id}/editar")
+    public ModelAndView atualizar(@PathVariable Integer id,
+                                  @RequestParam Map<String, Object> produto,
+                                  RedirectAttributes redirectAttributes) {
+        produtoService.atualizar(id, produto);
+
+        redirectAttributes.addFlashAttribute("mensagem", "Atualização feita com sucesso!");
+
+        return new ModelAndView("redirect:/produtos/{id}/editar");
+    }
+
+    @GetMapping("/{id}/editar")
+    public ModelAndView editar(@PathVariable Integer id) {
+        return novo(produtos.buscar(id));
+    }
+
+    @PostMapping("/novo")
+    public ModelAndView criar(Produto produto,
+                              BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        Produto atualizado = produtoService.criar(produto);
+
+        redirectAttributes.addFlashAttribute("mensagem", "Registro criado com sucesso!");
+
+        return new ModelAndView("redirect:/produtos/{id}/editar", "id", atualizado.getId());
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView novo(Produto produto) {
+        ModelAndView mv = new ModelAndView("produtos/produtos-formulario");
+        mv.addObject("produto", produto);
+
+        return mv;
+    }
 
     @GetMapping
     public ModelAndView listar() {
